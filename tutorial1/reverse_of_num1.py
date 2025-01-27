53a8248ac06d933337d4f3307d997bd3e0ba40ea
Poorvi0906 <poorvirbca24@rvu.edu.in>
2025-01-27 15:47:01 +0530

    Add files via upload

---
+++ tutorial1/reverse_of_num1.py
@@ -0,0 +1,12 @@
+
+t = n 
+rev =0 
+while n != 0:
+	print("Value of n is",n)
+	r = n % 10 
+	print("Value of r is",r)
+	rev =rev*10+r 
+	print("Value of rev is",rev)
+	n = n//10
+	print ("Value of n is", n)
+	print ("Reverse of a number",t,"is:", rev)
