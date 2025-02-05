896ef2e5d81488387c39639d0ce5c1aa26494d31
Poorvi0906 <poorvirbca24@rvu.edu.in>
2025-02-05 15:44:19 +0530

    Add files via upload

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -1,9 +1,11 @@
 def is_palindrome(string):
     left, right = 0, len(string) - 1
     while right >= left:
-        if string[left] != string[right]:
+        if string[left].upper() != string[right].upper():
             return False
         left += 1
         right -= 1
     return True
-print(is_palindrome('malayalam')) # Output: True
+print(is_palindrome('malayalam')) 
+string = input("Enter a string:")
+print(is_palindrome(string))
