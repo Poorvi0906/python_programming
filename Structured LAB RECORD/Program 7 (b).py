60362095d82fbe2786cec7793684459d886a53a1
Poorvi0906 <poorvirbca24@rvu.edu.in>
2025-04-09 06:24:02 +0530

    Update Program 7 (b).py

--- Structured LAB RECORD/Program 7 (b).py
+++ Structured LAB RECORD/Program 7 (b).py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df =pd.read_csv(r'C:\SHANTHI\RVU\Programs\Minors\IDA\Activities\Datasets\6Mcd_null_values.csv')
+df =pd.read_csv(r"C:\Users\poorv\Downloads\6Mcd_null_values.csv")
 
 df.fillna(df.mean(numeric_only=True), inplace=True)
 df.fillna({col: df[col].mode()[0] for col in df.columns if df[col].dtype == 'object'}, inplace=True)
