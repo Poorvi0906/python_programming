8213eb009027445ede7845c3d56f1c6f7f18e300
Poorvi0906 <poorvirbca24@rvu.edu.in>
2025-04-09 06:32:11 +0530

    Update Program 12 (a).py

--- Structured LAB RECORD/Program 12 (a).py
+++ Structured LAB RECORD/Program 12 (a).py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv(r'C:\Path\To\Your\File\4laptop.csv')
+df = pd.read_csv(r"C:\Users\poorv\Downloads\4laptops.csv")
 
 print("Dataset:")
 print(df.head())
